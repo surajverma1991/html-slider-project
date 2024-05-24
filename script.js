6f371396f6d428e9109fbbc5f37a85e82ca43b3f
Suraj Verma <vermasuraj833@gmail.com>
2024-05-24 10:03:53 +0530

    add border animation

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 $(document).ready(function () {
+  var slides = $(".slider > div");
   $(".slider").slick({
-    // autoplay: true,
-    autoplaySpeed: 500,
     prevArrow: false,
     nextArrow: false,
     slidesToShow: 3,
     centerMode: true,
     centerPadding: "60px",
+    speed: 5000,
+    autoplay: true,
+    infinite: true,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
           arrows: false,
           centerMode: true,
           centerPadding: "40px",
-          slidesToShow: 3,
+          slidesToShow: 2,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           arrows: false,
           centerMode: true,
@@ -28,4 +30,10 @@ $(document).ready(function () {
       },
     ],
   });
+
+  $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $('.slick-slide').removeClass('slick-prev slick-next');
+    $('.slick-slide[data-slick-index="' + currentSlide + '"]').addClass('slick-prev');
+    $('.slick-slide[data-slick-index="' + nextSlide + '"]').addClass('slick-next');
+});
 });
